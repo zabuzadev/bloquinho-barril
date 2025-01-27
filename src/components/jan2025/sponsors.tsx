eeeda6ee91c9d90d12ce4d585596396346bbf59e
AES MARKETING LTDA <infra@aesmkt.com.br>
2025-01-27 20:05:11 -0300

    Update sponsors.tsx

--- src/components/jan2025/sponsors.tsx
+++ src/components/jan2025/sponsors.tsx
@@ -4,12 +4,22 @@ export default function Sponsors() {
     return (
       <div className="py-16 bg-white">
         <h2 className="text-center text-2xl font-bold mb-8">Patrocinadores Oficiais</h2>
-        <div className="flex justify-center gap-8">
-          <div className="w-48 h-32  flex items-center justify-center">
-           <Image src="/images/aes.png" alt="Patrocinador" width={150} height={150} />
+        <div className="flex justify-center flex-wrap gap-8">
+            <div className="w-48 h-32 flex  items-center justify-center">
+              <Image src="/images/protrocinadores/01.png" alt="Patrocinador" width={150} height={150} />
+            </div>
+            <div className="w-48 h-32 flex  items-center justify-center">
+              <Image src="/images/protrocinadores/02.png" alt="Patrocinador" width={150} height={150} />
+            </div>
+            <div className="w-48 h-32 flex  items-center justify-center">
+              <Image src="/images/protrocinadores/03.png" alt="Patrocinador" width={150} height={150} />
+            </div>
+            <div className="w-48 h-32 flex  items-center justify-center">
+              <Image src="/images/protrocinadores/04.png" alt="Patrocinador" width={150} height={150} />
+            </div>
           </div>
         </div>
-      </div>
+
     )
   }
   
